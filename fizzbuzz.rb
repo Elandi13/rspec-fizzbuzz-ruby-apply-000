9ca0735e00540138d34f9a3c5234468683b377cb
Elvis Landi <elandi13@yahoo.com>
2020-09-19 18:33:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(x)
+  if x % 5 == 0 and x % 3== 0
+    puts "Fizzbuzz"
+    elsif x % 5 == 0
+    puts "Buzz"
+    elsif x % 3 == 0
+    puts "Fizz"
+  else
+    puts "int"
+  end
+end
