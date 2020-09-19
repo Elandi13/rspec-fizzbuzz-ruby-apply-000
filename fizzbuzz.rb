b48a6c9d516336e7e5c816a93300de9b97da9084
Elvis Landi <elandi13@yahoo.com>
2020-09-19 18:56:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,4 @@
 def fizzbuzz(x)
-  if x % 3 == 0 
-    "Fizz"
-  
-  elsif x % 5 == 0 
-  "Buzz"
-  
-  elsif x % 3 == 0 && x % 5 == 0
-  "Fizzbuzz"
-end
-end
+  if x % 3 == 0 && x % 5 == 0
+    "Fizzbuzz"
+    
